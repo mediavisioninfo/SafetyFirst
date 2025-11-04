c06d9594e31c1e632483156abc83b19a91810be3
tanujaMediaVision <tanuja@mediavisioninfo.com>
2025-11-04 12:45:29 +0530

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -89,21 +89,21 @@ class AuthenticatedSessionController extends Controller
         ]);
 
         // Send Email OTP
-        // if (!empty($user->email)) {
-        //     Mail::send('auth.email-otp', [
-        //         'name' => $user->name,
-        //         'otp' => $otp
-        //     ], function ($message) use ($user) {
-        //         $message->to($user->email)
-        //             ->subject('Your OTP Code - Safety First');
-        //     });
-        // }
+        if (!empty($user->email)) {
+            Mail::send('auth.email-otp', [
+                'name' => $user->name,
+                'otp' => $otp
+            ], function ($message) use ($user) {
+                $message->to($user->email)
+                    ->subject('Your OTP Code - Safety First');
+            });
+        }
 
         // dd($user->phone_number);
 
         // Send SMS OTP
         if (!empty($user->phone_number)) {
-            dd($user->phone_number);
+            // dd($user->phone_number);
             $authKey = env('SMSCOUNTRY_AUTHKEY');
             $authToken = env('SMSCOUNTRY_AUTHTOKEN');
             $senderId = env('SMSCOUNTRY_SENDERID');
